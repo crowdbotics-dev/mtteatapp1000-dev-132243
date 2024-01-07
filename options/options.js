4d49d1b16a039161bc7b126a3f24af359e34b6b7
Crowdbotics <engineering@crowdbotics.com>
2024-01-07 14:41:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mtteatapp1000-dev-132243",
+  url: "https://mtteatapp1000-dev-132243.botics.co",
+  api: "https://mtteatapp1000-dev-132243.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
